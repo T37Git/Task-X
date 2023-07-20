19a752e15dd366c8a08dd69ef98aafb8c174b641
T37Git <140069600+T37Git@users.noreply.github.com>
2023-07-20 07:49:34 -0700

    Add HelloWorld & HelloITI

---
+++ HelloITI.java
@@ -0,0 +1,10 @@
+ public class HelloITI{
+    public void printHelloITI() {
+        System.out.println("Hello iTi");
+    }
+
+    public static void main(String[] args) {
+        HelloITI hello = new HelloITI();
+        hello.printHelloITI();
+    }
+}
