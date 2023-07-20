4c57cd097b0b6c54604e570a7b5d72659e542ca9
T37Git <140069600+T37Git@users.noreply.github.com>
2023-07-20 08:20:38 -0700

    HelloWorld.java

--- HelloITI.java
+++ HelloITI.java
@@ -3,8 +3,14 @@
         System.out.println("Hello iTi");
     }
 
+    public void printHelloGit() {
+        System.out.println("Hello Git");
+    }
+
+
     public static void main(String[] args) {
         HelloITI hello = new HelloITI();
         hello.printHelloITI();
+	hello.printHelloGit();
     }
 }
