09fa8f76b7edbf75c44b83612619f8b2582895dc
T37Git <140069600+T37Git@users.noreply.github.com>
2023-07-20 08:40:46 -0700

    Rename HelloWorld to HelloJava, file & class

---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava{
+    public void printHelloJava() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        HelloJava hello = new HelloJava();
+        hello.printHelloJava();
+    }
+}
